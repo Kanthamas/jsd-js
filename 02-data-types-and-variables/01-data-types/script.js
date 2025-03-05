f6c03dbde7c6b8baa899452fc73ed38c7d471618
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-03-06 04:30:08 +0700

    Add 02-data-types-and-variables

---
+++ 02-data-types-and-variables/01-data-types/script.js
@@ -0,0 +1,69 @@
+console.log(1);
+
+/* // Google Chrome Dev Tool: Memory
+const myShoppingList = ["milk", "coffee", "cake"];
+const yourShoppingList = myShoppingList;
+const johnShoppingList = ["cat food", "pizza", "coffee"];
+
+console.log(myShoppingList);
+console.log(yourShoppingList);
+console.log(johnShoppingList); */
+
+
+
+/* let num1 = 1
+console.log(num1)
+console.log(typeof num1)
+
+let num2;
+console.log(num2)
+console.log(typeof num2)
+
+let num3 = null;
+console.log(num3);
+console.log(typeof num3);
+
+let sum12 = num1 + num2 // → 1 + undefined(NaN) → NaN
+console.log(sum12)
+
+let sum13 = num1 + num3 // → 1 + null(0) → 1 + 0 → 1
+console.log(sum13)
+
+let sum23 = num2 + num3 // → undefined + null → NaN + 0 → NaN
+console.log(sum23)
+ */
+
+
+// let myName = "kanthamas";
+// let myFirstName = myName;
+// let myAnotherName = "kanthamas";
+
+// console.log("myName : ", myName);
+// console.log("myFirstName; ", myFirstName);
+// console.log("myAnotherName: ", myAnotherName);
+
+// console.log("typeof myName : ", typeof myName);
+// console.log("typeof myFirstName; ", typeof myFirstName);
+// console.log("typeof myAnotherName: ", typeof myAnotherName);
+
+// console.log(myName[0])
+// console.log(myName[1])
+// console.log(myName[2])
+// console.log(myName[3])
+// console.log(myName[4])
+
+// myName[4] = "Z"
+// console.log(myName[4])
+
+// console.log("*".repeat(30));
+// myName = "MEANZILLA";
+// console.log("myName : ", myName);
+// console.log("myFirstName; ", myFirstName);
+// console.log("myAnotherName: ", myAnotherName);
+
+// console.log("*".repeat(30));
+// myFirstName = myName
+// myAnotherName = myFirstName
+// console.log("myName : ", myName);
+// console.log("myFirstName; ", myFirstName);
+// console.log("myAnotherName: ", myAnotherName);
