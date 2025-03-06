acaed10817cff992fa0191b24248166a9e988b56
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-03-06 15:36:52 +0700

    Update 02-data-types-and-variables

--- 02-data-types-and-variables/01-data-types/script.js
+++ 02-data-types-and-variables/01-data-types/script.js
@@ -1,4 +1,72 @@
-console.log(1);
+const arr1 = [1,2,3]
+// console.log(arr1)
+// console.log(arr1[0])
+// console.log(arr1[1])
+// console.log(arr1[2])
+// console.log(arr1[3])
+console.log()
+
+
+console.log(typeof arr1)
+
+/* const user = {
+	userName: "john",
+	isAdmin: true,
+	age: 200,
+	food: ["pizza", "apple"],
+};
+
+const user2 = user;
+
+// console.log(user)
+// console.log(user2)
+
+user2.userName = "mark";
+// console.log(user2.userName)
+
+console.log(user);
+console.log(user2);
+
+const user3 = { ...user };
+// console.log(user3)
+
+user3.userName = "mean";
+console.log(user3); */
+
+
+
+
+// var num1 = 1
+// console.log(num1)
+
+// var num2;
+// console.log(num2)
+
+// console.log(num3);
+// var num3 = 3;
+// console.log(num3);
+
+// const pi = 3.1415926
+// console.log(pi)
+
+// console.log(pi)
+// const pi = 3.1415926;
+
+// console.log(1);
+
+/* let num = 5;
+num = 10;
+console.log(num);
+
+let myName = "Mean"
+myName = "Kanthamas"
+
+console.log(myName)
+
+console.log(myName[0])
+
+myName[0] ="C";
+console.log(myName[0]); */ // Canthamas
 
 /* // Google Chrome Dev Tool: Memory
 const myShoppingList = ["milk", "coffee", "cake"];
@@ -8,8 +76,6 @@ const johnShoppingList = ["cat food", "pizza", "coffee"];
 console.log(myShoppingList);
 console.log(yourShoppingList);
 console.log(johnShoppingList); */
-
-
 
 /* let num1 = 1
 console.log(num1)
@@ -33,7 +99,6 @@ let sum23 = num2 + num3 // → undefined + null → NaN + 0 → NaN
 console.log(sum23)
  */
 
-
 // let myName = "kanthamas";
 // let myFirstName = myName;
 // let myAnotherName = "kanthamas";
@@ -45,15 +110,6 @@ console.log(sum23)
 // console.log("typeof myName : ", typeof myName);
 // console.log("typeof myFirstName; ", typeof myFirstName);
 // console.log("typeof myAnotherName: ", typeof myAnotherName);
-
-// console.log(myName[0])
-// console.log(myName[1])
-// console.log(myName[2])
-// console.log(myName[3])
-// console.log(myName[4])
-
-// myName[4] = "Z"
-// console.log(myName[4])
 
 // console.log("*".repeat(30));
 // myName = "MEANZILLA";
---
+++ 02-data-types-and-variables/02-variables/script.js
@@ -0,0 +1,59 @@
+
+/* let num1 = undefined
+let num2 = 555
+
+let sum12 = num1 + num2
+
+console.log(typeof num1)
+console.log(typeof num2)
+
+console.log(sum12)
+console.log(typeof sum12)
+
+let num3 = Number.MAX_SAFE_INTEGER
+console.log(num3)
+console.log(num3 + 6) */
+
+
+
+
+
+
+/* let firstName = "kanthamas";
+
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = 555;
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = true;
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = null;
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = undefined;
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = {};
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = []
+console.log(firstName);
+console.log(typeof firstName);
+
+function greet() {
+  return "hello"
+}
+
+console.log(greet())
+
+firstName = new Date()
+console.log(firstName);
+console.log(typeof firstName); */
