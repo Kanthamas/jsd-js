281776ebec1f0dce36dfcae6abc58ee7d0b6bf9b
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-03-09 00:39:50 +0700

    Add 04-functions > 01-use-strict, and template(index.html and script.js)

---
+++ 04-functions-and-scopes/01-use-strict/script.js
@@ -0,0 +1,38 @@
+// Without "use strict"
+/* num1 = 1;
+console.log(num1);
+
+num1 = "one";
+console.log(num1);
+
+myToolbox = {
+	html: true,
+	css: true,
+	javascript: true,
+	react: false,
+	node: false,
+	sql: true,
+	mongodb: false,
+};
+console.log(myToolbox.html);
+delete myToolbox;
+// console.log(myToolbox.html); */
+
+// With "use strict"
+// "use strict";
+// num2 = 2;
+// console.log(num2);
+
+// myToolbox = {
+// 	html: true,
+// 	css: true,
+// 	javascript: true,
+// 	react: false,
+// 	node: false,
+// 	sql: true,
+// 	mongodb: false,
+// };
+// console.log(myToolbox.html);
+
+// delete myToolbox;
+// console.log(myToolbox.html);
