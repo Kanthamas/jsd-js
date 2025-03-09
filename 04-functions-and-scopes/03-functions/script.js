a3d2387e4a16ca429f48e1d1ba18c43da2687d1b
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-03-09 21:52:12 +0700

    Add Function Types Examples

--- 04-functions-and-scopes/03-functions/script.js
+++ 04-functions-and-scopes/03-functions/script.js
@@ -1,1 +1,76 @@
-"use strict"
+"use strict";
+
+// Function: Callback Functions
+/* function fetchData(callback) {
+	setTimeout(() => {
+		console.log("Data fetched!");
+		callback();
+	}, 2000);
+}
+
+fetchData(() => {
+	console.log("Callback executed after data is fetched.");
+}); */
+
+// Function: Anonymous Functions
+/* setTimeout(function () {
+	console.log("This is an anonymous function! 😎");
+}, 1000);
+
+setTimeout(() => {
+	console.log("This is another anonymous function! 😄");
+}, 3000); */
+
+// Function: IIFE
+/* (function () {
+	let message = "I am local ⚡";
+	console.log(message);
+})(); */
+
+// Function: Higher-Order Functions
+/* const numbers = [1, 2, 3, 4, 5];
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+
+console.log(evenNumbers); 
+ */
+// Function: Function Factory
+/* function multiplier(factor) {
+	return function (number) {
+		return number * factor;
+	};
+}
+
+const multiplyBy2 = multiplier(2);
+console.log(multiplyBy2(10)); 
+
+const multiplyBy99Point99 = multiplier(99.99);
+console.log(multiplyBy99Point99(10));  */
+
+// Generator Functions
+/* function* numberGenerator(limit = Infinity) {
+	let num = 1;
+	while (num <= limit) {
+		yield num++;
+	}
+}
+
+const globalNumbers = numberGenerator();
+console.log(`Global Counter: ${globalNumbers.next().value}`);
+console.log(`Global Counter: ${globalNumbers.next().value}`);
+console.log(`Global Counter: ${globalNumbers.next().value}`);
+console.log(`Global Counter: ${globalNumbers.next().value}`);
+console.log(`Global Counter: ${globalNumbers.next().value}`);
+
+const counter1 = numberGenerator();
+console.log(`counter1: ${counter1.next().value}`);
+console.log(`counter1: ${counter1.next().value}`);
+console.log(`counter1: ${counter1.next().value}`);
+
+console.log(`Global Counter: ${globalNumbers.next().value}`);
+console.log(`Global Counter: ${globalNumbers.next().value}`);
+
+const limitedNumbers = numberGenerator(5);
+for (let num of limitedNumbers) {
+	console.log(num);
+} */
