91a04e81e5a9251ff9982ceb14ddf23ad7d6214e
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-03-11 00:48:17 +0700

    Add 04-functions > 05-demo > demo-02-math-random

--- 04-functions-and-scopes/05-demo/demo-02-math-random/script.js
+++ 04-functions-and-scopes/05-demo/demo-02-math-random/script.js
@@ -3,3 +3,15 @@
 // Requirement: Random Number Function
 // Create a function that accepts one number and generates a random number from 1 to that number.
 
+function generateRandomNumber(max) {
+	if (typeof max === "number" && max > 1) {
+		return Math.floor(Math.random() * max) + 1;
+	}
+	return `Invalid input.`;
+}
+
+console.log(generateRandomNumber(5)); // 1 -> 5
+console.log(generateRandomNumber(0));
+console.log(generateRandomNumber("3"));
+console.log(generateRandomNumber(NaN));
+console.log(generateRandomNumber());
